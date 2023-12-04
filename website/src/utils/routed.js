4cb96dca6cf12fdc52633f1d3755b3e7d9263b20
Ivan Chizhov <chizhov@me.com>
2023-12-03 21:25:00 -0500

    Fixes non routed navigation on icon click

--- website/src/utils/routed.js
+++ website/src/utils/routed.js
@@ -2,12 +2,19 @@ import React from 'react'
 import {useNavigate} from 'react-router-dom'
 
 
+const findHref = (element) => {
+  if (!element) return null
+  if (element.tagName === 'A') return element.href
+  return findHref(element.parentElement)
+}
+
 const routed = (WrappedComponent, replace = false) => {
   return React.forwardRef((props, ref) => {
     const navigate = useNavigate()
     const handleClick = (e) => {
-      if (e.target.href) {
-        const url = new URL(e.target.href)
+      const href = findHref(e.target)
+      if (href) {
+        const url = new URL(href)
         if (url.origin === window.location.origin) {
           e.preventDefault()
           navigate(url.pathname, {replace})
